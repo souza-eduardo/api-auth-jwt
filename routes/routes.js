edfdc9bbf848f03fb2c58fd003076a64ec84881d
souza-eduardo <esouzasantos085@gmail.com>
2024-02-10 10:19:06 -0400

    created routes and controller

--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,17 @@ const router = express.Router();
 const User = require('../models/user');
 
 router.post('/', async (req, res) => {
-  
+
+  const user =  new User ({
+    userName: req.body.userName,
+    email: req.body.email,
+    password: req.body.password
+  });
+  try {
+    await user.save();
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 router.get('/', async (req, res) => {
